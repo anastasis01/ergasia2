f4d37addbd11c38cc9bbddd429c0f78130ca120e
anastasis01 <100617025+anastasis01@users.noreply.github.com>
2023-05-31 01:30:03 +0300

    Add files via upload

---
+++ gr.tasos.junit.first/src/main/java/gr/tasos/junit/first/MyMain.java
@@ -0,0 +1,12 @@
+package gr.tasos.junit.first;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		MyClass m=new MyClass();
+		String result=m.sub(10,5);
+		
+        System.out.println("result is "+result);
+	}
+
+}
